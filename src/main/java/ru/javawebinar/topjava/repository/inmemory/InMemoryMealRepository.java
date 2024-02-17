3e2a031a261062ee48dbba360427a6dc20d0e875
Roman <dubrovin.skfo@gmail.com>
2024-02-17 22:25:44 +0300

    bug fix

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,12 +1,14 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -29,21 +31,16 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
+        Map<Integer, Meal> meals = repository.computeIfAbsent(userId, ConcurrentHashMap::new);
         meal.setUserId(userId);
         if (meal.isNew()) {
-            if (!repository.containsKey(userId)) {
-                repository.put(userId, new ConcurrentHashMap<>());
-            }
             meal.setId(counter.incrementAndGet());
-            repository.computeIfPresent(userId, (id, oldMap) -> {
-                oldMap.put(meal.getId(), meal);
-                return oldMap;
-            });
+            meals.put(meal.getId(), meal);
             return meal;
         }
 
         // handle case: update, but not present in storage
-        return get(meal.getId(), userId) == null ? null : repository.get(userId).computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return get(meal.getId(), userId) == null ? null : meals.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
     @Override
@@ -69,10 +66,11 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     private List<Meal> filterByPredicate(int userId, Predicate<Meal> filter) {
-        Predicate<Meal> defaultUserIdFilter = meal -> meal.getUserId() == userId;
-        return repository.get(userId).values()
+        Map<Integer, Meal> meals = repository.get(userId);
+        return CollectionUtils.isEmpty(meals) ? Collections.emptyList() :
+                meals.values()
                 .stream()
-                .filter(defaultUserIdFilter.and(filter))
+                .filter(filter)
                 .sorted(Comparator.comparing(Meal::getDateTime).reversed())
                 .collect(Collectors.toList());
     }
