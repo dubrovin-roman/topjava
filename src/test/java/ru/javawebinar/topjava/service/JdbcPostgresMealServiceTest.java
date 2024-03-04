b26ab672f93b3dc308ecbd2750d80114342e919c
Roman Dubrovin <dubrovin.skfo@gmail.com>
2024-03-04 23:58:48 +0300

    extends AbstractMealServiceTest

--- src/test/java/ru/javawebinar/topjava/service/JdbcPostgresMealServiceTest.java
+++
@@ -1,7 +0,0 @@
-package ru.javawebinar.topjava.service;
-
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles(profiles = {"jdbc", "postgres"})
-public class JdbcPostgresMealServiceTest extends AbstractMealServiceTest{
-}
