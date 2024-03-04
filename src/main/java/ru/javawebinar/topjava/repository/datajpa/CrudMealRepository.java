b0964ddd82ed0abd48e658532f0d9051434ef196
Roman Dubrovin <dubrovin.skfo@gmail.com>
2024-03-04 22:51:19 +0300

    impl DataJpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,31 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Query(nativeQuery = true, value = "SELECT * FROM meal WHERE id=:id AND user_id=:userId")
+    Optional<Meal> findByIdAndUserId(@Param("id") int id, @Param("userId") int userId);
+
+    @Query(nativeQuery = true, value = "DELETE FROM meal WHERE id=:id AND user_id=:userId")
+    @Modifying
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM meal WHERE user_id=:userId ORDER BY date_time DESC")
+    List<Meal> findAllByUserId(@Param("userId") int userId);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM meal WHERE user_id=:userId " +
+            "AND date_time>=:startDateTime " +
+            "AND date_time<:endDateTime " +
+            "ORDER BY date_time DESC")
+    List<Meal> findBetweenHalfOpen(@Param("startDateTime") LocalDateTime startDateTime,
+                                   @Param("endDateTime") LocalDateTime endDateTime,
+                                   @Param("userId") int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,43 +1,56 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class DataJpaMealRepository implements MealRepository {
 
-    private final CrudMealRepository crudRepository;
+    private final CrudMealRepository crudMealRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
-        this.crudRepository = crudRepository;
+    public DataJpaMealRepository(CrudMealRepository crudMealRepository, CrudUserRepository crudUserRepository) {
+        this.crudMealRepository = crudMealRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+        User ref = crudUserRepository.getReferenceById(userId);
+        meal.setUser(ref);
+        if (meal.isNew()) {
+            return crudMealRepository.save(meal);
+        } else {
+            return get(meal.getId(), userId) != null ? crudMealRepository.save(meal) : null;
+        }
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return crudMealRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudMealRepository.findByIdAndUserId(id, userId).orElse(null);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudMealRepository.findAllByUserId(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudMealRepository.findBetweenHalfOpen(startDateTime, endDateTime, userId);
     }
 }
