5ae7158e3b16db0319c06cfa5e064095283aab55
Roman Dubrovin <dubrovin.skfo@gmail.com>
2024-05-23 16:58:21 +0300

    bug fix

--- src/main/java/ru/javawebinar/topjava/util/UserValidator.java
+++ src/main/java/ru/javawebinar/topjava/util/UserValidator.java
@@ -33,8 +33,7 @@ public class UserValidator implements Validator {
         if (userFromDB != null) {
             if (requestURI.contains("admin")) {
                 if (!user.isNew()) {
-                    if ((SecurityUtil.authUserId() == user.getId() && !Objects.equals(user.getId(), userFromDB.getId()))
-                            || Objects.equals(user.getId(), userFromDB.getId())) {
+                    if (Objects.equals(user.getId(), userFromDB.getId())) {
                         return;
                     }
                 }
